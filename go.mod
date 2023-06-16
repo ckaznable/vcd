fe80e04d150600149731803db39b82e2073c5a5e
ckaznable <kami25565@gmail.com>
2023-06-16 14:48:04 +0800

    feat: add tui impl

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/ckaznable/vcd
 
 go 1.20
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.6.0
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/term v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/ckaznable/vcd/nv"
+	"github.com/gdamore/tcell/v2"
 )
+
+type Date struct {
+	Day     int
+	Hours   int
+	Minutes int
+}
 
 func main() {
 	now := time.Now()
@@ -36,15 +45,136 @@ func main() {
 		}
 	}
 
-	cmp(now, nextVacation)
+	nextDate := diff(now, nextVacation)
+
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	// Set default text style
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	s.SetStyle(defStyle)
+
+	// Clear screen
+	s.Clear()
+
+	ch := make(chan Date)
+
+	quit := func() {
+		s.Fini()
+		os.Exit(0)
+	}
+
+	go func() {
+		first := true
+		now := time.Now()
+		remainingSeconds := 60 - now.Second()
+
+		if now.Second() == 0 {
+			first = false
+		}
+
+		ch <- nextDate
+		for {
+			if first {
+				first = false
+				time.Sleep(time.Duration(remainingSeconds) * time.Second)
+			} else {
+				time.Sleep(time.Minute * 1)
+			}
+
+			nextDate = tick(nextDate)
+			ch <- nextDate
+		}
+	}()
+
+	go func() {
+		for {
+			// Poll event
+			ev := s.PollEvent()
+
+			// Process event
+			switch ev := ev.(type) {
+			case *tcell.EventResize:
+				s.Sync()
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+					quit()
+				}
+			}
+		}
+	}()
+
+	for {
+		select {
+		case nextDate = <-ch:
+		default:
+		}
+
+		draw(s, nextDate)
+		time.Sleep(time.Second)
+	}
 }
 
-func cmp(time1 time.Time, time2 time.Time) {
+func diff(time1 time.Time, time2 time.Time) Date {
 	duration := time2.Sub(time1)
 
 	days := int(duration.Hours() / 24)
 	hours := int(duration.Hours()) % 24
 	minutes := int(duration.Minutes()) % 60
 
-	fmt.Printf("%d:%d:%d\n", days, hours, minutes)
+	return Date{
+		Day:     days,
+		Hours:   hours,
+		Minutes: minutes,
+	}
+}
+
+var tickFlag = true
+
+func draw(s tcell.Screen, d Date) {
+	joinStr := ":"
+	if tickFlag {
+		tickFlag = false
+	} else {
+		joinStr = " "
+		tickFlag = true
+	}
+
+	str := fmt.Sprintf("%d%s%0*d%s%0*d", d.Day, joinStr, 2, d.Hours, joinStr, 2, d.Minutes)
+
+	width, height := s.Size()
+	y := height / 2
+	x := width/2 - len(str)/2
+
+	s.Clear()
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	for i, char := range str {
+		s.SetContent(x+i, y, char, nil, defStyle)
+	}
+
+	s.Show()
+}
+
+func tick(d Date) Date {
+	d.Minutes--
+	if d.Minutes == -1 {
+		d.Minutes = 59
+		d.Hours--
+	}
+
+	if d.Hours == -1 {
+		d.Hours = 23
+		d.Day--
+	}
+
+	if d.Day == -1 {
+		d.Day = 0
+	}
+
+	return d
 }
